cc796fdfbc583138b39445572fd3addd4f56e38c
Cladlink <michael.boutboul87@gmail.com>
2017-12-13 13:21:52 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Ma première superbe application en angular';
 }
